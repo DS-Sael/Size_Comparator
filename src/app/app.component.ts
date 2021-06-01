53e4d31243c2e50610afe93b2b69a436cc2b3b2e
Sael <>
2021-06-01 09:53:00 +0200

    Correction interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,21 +20,21 @@ export class AppComponent implements OnInit {
     this.characterA = new FormGroup({});
     this.characterA.addControl('character-name',new FormControl(this.checkNull(localStorage.getItem('character-nameA'))));
     this.characterA.addControl('normal-size',new FormControl(this.checkNull(localStorage.getItem('normal-sizeA'))));
-    this.characterA.addControl('normal-measure',new FormControl(this.checkNull(localStorage.getItem('normal-measureA'))));
+    this.characterA.addControl('normal-measure',new FormControl(this.checkNull(localStorage.getItem('normal-measureA'),"m")));
     this.characterA.addControl('changed-size',new FormControl(this.checkNull(localStorage.getItem('changed-sizeA'))));
-    this.characterA.addControl('changed-measure',new FormControl(this.checkNull(localStorage.getItem('changed-measureA'))));
+    this.characterA.addControl('changed-measure',new FormControl(this.checkNull(localStorage.getItem('changed-measureA'),"m")));
     this.characterB = new FormGroup({});
     this.characterB.addControl('character-name',new FormControl(this.checkNull(localStorage.getItem('character-nameB'))));
     this.characterB.addControl('normal-size',new FormControl(this.checkNull(localStorage.getItem('normal-sizeB'))));
-    this.characterB.addControl('normal-measure',new FormControl(this.checkNull(localStorage.getItem('normal-measureB'))));
+    this.characterB.addControl('normal-measure',new FormControl(this.checkNull(localStorage.getItem('normal-measureB'),"m")));
     this.characterB.addControl('changed-size',new FormControl(this.checkNull(localStorage.getItem('changed-sizeB'))));
-    this.characterB.addControl('changed-measure',new FormControl(this.checkNull(localStorage.getItem('changed-measureB'))));
+    this.characterB.addControl('changed-measure',new FormControl(this.checkNull(localStorage.getItem('changed-measureB'),"m")));
 
   }
 
-  checkNull(toCheck: string){
+  checkNull(toCheck: string, defaultValue = ""){
     if(toCheck == "null"){
-      return "";
+      return defaultValue;
     }
     else{
       return toCheck;
@@ -53,7 +53,16 @@ export class AppComponent implements OnInit {
     localStorage.setItem('changed-sizeB', this.characterB.value['changed-size']);
     localStorage.setItem('changed-measureB', this.characterB.value['changed-measure']);
     let result = Calculation.compare(this.characterA.value['normal-size'], this.characterA.value['normal-measure'], this.characterA.value['changed-size'], this.characterA.value['changed-measure'], this.characterB.value['normal-size'], this.characterB.value['normal-measure'], this.characterB.value['changed-size'], this.characterB.value['changed-measure'])
-    this.descCharacterA = `Pour ${this.characterA.value['character-name']}, ${this.characterB.value['character-name']} mesure ${result[0].val}${result[0].unit}`;
-    this.descCharacterB = `Pour ${this.characterB.value['character-name']}, ${this.characterA.value['character-name']} mesure ${result[1].val}${result[1].unit}`;
+    let valueA = result[0].val;
+    let valueB = result[1].val;
+    debugger;
+    if(Math.floor(valueA) != valueA){
+      valueA = valueA.toFixed(2);
+    }
+    if(Math.floor(valueB) != valueB){
+      valueB = valueB.toFixed(2);
+    }
+    this.descCharacterA = `Pour ${this.characterA.value['character-name']}, ${this.characterB.value['character-name']} mesure ${valueA}${result[0].unit}`;
+    this.descCharacterB = `Pour ${this.characterB.value['character-name']}, ${this.characterA.value['character-name']} mesure ${valueB}${result[1].unit}`;
   }
 }
